4450ae8549e0093c8c27dede3a0ce974dec5e921
Sergey Gorshkov <gorshkov.s@extyl-pro.ru>
2025-07-15 15:43:44 +0300

    fix changes

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -5,5 +5,12 @@ return [
     'Remember_me' => 'Remember me',
     'Forgot your password' => 'Forgot your password?',
     'Log in' => 'Log in',
-
+    'errors_found' => 'Oops! Something went wrong:',
+    'email_password_reset_link' => 'Reset password',
+    'Already registered?' => 'Already registered?',
+    'Register' => 'Register',
+    'Create' => 'Create',
+    'Update' => 'Update',
+    'Edit' => 'Edit',
+    'Delete' => 'Delete',
 ];
