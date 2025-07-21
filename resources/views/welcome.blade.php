216ea45a00da0aa5ab4bbcce5ac5d8f40e8c69ad
Sergey Gorshkov <gorshkov.s@extyl-pro.ru>
2025-07-21 14:59:19 +0300

    fix click me

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
             <h1 class="display-4 fw-bold mb-3">Привет от Хекслета!</h1>
             <p class="lead text-muted mb-4">Это простой менеджер задач на Laravel</p>
-            <a href="#" class="btn btn-outline-dark">Нажми меня</a>
+            <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Нажми меня</a>
         </div>
     </div>
 @endsection
